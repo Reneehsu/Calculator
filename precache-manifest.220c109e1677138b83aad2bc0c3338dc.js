cfa560962e749f423656e6f8dc387b7692814b1c
git push -u origin <renee8688@gmail.com>
2018-12-22 10:20:14 +0800

    Updates

---
+++ precache-manifest.220c109e1677138b83aad2bc0c3338dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "546deb05c16eeec89d41",
+    "url": "/Calculator/static/js/runtime~main.546deb05.js"
+  },
+  {
+    "revision": "0b4fbab7882e5535768b",
+    "url": "/Calculator/static/js/main.0b4fbab7.chunk.js"
+  },
+  {
+    "revision": "6f47d7b7079c3e74bf94",
+    "url": "/Calculator/static/js/1.6f47d7b7.chunk.js"
+  },
+  {
+    "revision": "0b4fbab7882e5535768b",
+    "url": "/Calculator/static/css/main.cc569f25.chunk.css"
+  },
+  {
+    "revision": "39c61b62cbcb669d1d5f9c49258c6c9a",
+    "url": "/Calculator/index.html"
+  }
+];
